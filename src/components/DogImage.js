150af7ca5d031fe09659a0f56746a7094f9b379e
Antonio <antonio18casta@aol.com>
2022-11-01 17:42:56 -0400

    commit tree 3b8419ac7ae83e0933debb784333d180ca8e0fe2

---
+++ src/components/DogImage.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+
+const API_URL = "https://dog.ceo/api/breeds/image/random"
+
+function DogImage() {
+    const [image, setImage] = useState(null)
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+            .then(r => r.json())
+            .then(data => setImage(data.message))
+    }, [])
+
+    if (!image) {
+        return <p>Loading...</p>
+    }
+
+    return (
+        <div>
+            <img src={image} alt="A Random Dog" />
+        </div>
+    );
+}
+
+export default DogImage
